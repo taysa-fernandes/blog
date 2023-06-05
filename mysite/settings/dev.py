71544568cde496fc3e0079fbce5fe5303f4ce273
taysa-fernandes <taysamirelly34@gmail.com>
2023-06-05 09:51:32 -0300

    blog inicial

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "$ot*3(-^uf&^+5tqy3iiw1_7%4l97)f@0+mt2h(*x+##f0-(8v"
+
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local import *  # noqa
+except ImportError:
+    pass
---
+++ mysite/settings/prod.py
@@ -0,0 +1,39 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "$ot*3(-^uf&^+5tqy3iiw1_7%4l97)f@0+mt2h(*x+##f0-(8v"
+
+# Add your site's domain name(s) here.
+ALLOWED_HOSTS = ["localhost"]
+
+# To send email from the server, we recommend django_sendmail_backend
+# Or specify your own email backend such as an SMTP server.
+# https://docs.djangoproject.com/en/3.2/ref/settings/#email-backend
+# EMAIL_BACKEND = "django_sendmail_backend.backends.EmailBackend"
+
+# Default email address used to send messages from the website.
+DEFAULT_FROM_EMAIL = "mysite <info@localhost>"
+
+# A list of people who get error notifications.
+ADMINS = [
+    ("Administrator", "admin@localhost"),
+]
+
+# A list in the same format as ADMINS that specifies who should get broken link
+# (404) notifications when BrokenLinkEmailsMiddleware is enabled.
+MANAGERS = ADMINS
+
+# Email address used to send error messages to ADMINS.
+SERVER_EMAIL = DEFAULT_FROM_EMAIL
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
+        "LOCATION": os.path.join(BASE_DIR, "cache"),  # noqa
+        "KEY_PREFIX": "coderedcms",
+        "TIMEOUT": 14400,  # in seconds
+    }
+}
